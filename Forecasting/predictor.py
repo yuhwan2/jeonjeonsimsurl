391b72529be864469aff2599d8a238bfe4084a8e
이유환 <yuhwan@iyuhwan-ui-MacBookAir.local>
2023-09-14 16:23:55 +0900

    first commit

---
+++ Forecasting/predictor.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import os as os
+
+
+def TargetDataReader(loc, Filename) :
+
+    Path = os.path.join(loc, Filename)
+    TempWeather = pd.read_csv(Path)
+    TempWeather["DeliveryDT"] = pd.to_datetime(TempWeather["DeliveryDT"],format = '%Y-%m-%d %H:%M:%S', utc = False)
+
+    return TempWeather
---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from Forecasting import predictor as pr
+import params as pa
+
+if __name__ == '__main__' :
+    File = "Solar_4.csv"
+    Solar = pr.TargetDataReader(pa.loc, File)
